f153ff25fd3927650c21eebfde4dac245287f91d
robert <robert.yifan.wang@gmail.com>
2013-04-09 15:55:27 -0400

    Android App init

---
+++ Client/src/se/yifan/rna/RnAActivity.java
@@ -0,0 +1,15 @@
+package se.yifan.rna;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RnAActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
